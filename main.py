939a4eef8cc192d15446154f61f1764c0a912de9
RigalHD <profoundromeo@gmail.com>
2024-07-01 14:34:54 +0300

    Add(main.py): after start bot prints to console selected chat history and members of it

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pyrogram import Client
+from pyrogram.types import Message, User
 from dotenv import load_dotenv
 from os import getenv
 import asyncio
@@ -10,8 +11,13 @@ api_hash = getenv("api_hash")
 
 async def main():
     async with Client("my_account", api_id, api_hash) as app:
-        pass
-        # await app.send_message("me", "Greetings from **Pyrogram**!")
+        async for message in app.get_chat_history("-4210829020"):
+            print(
+                f"{message.from_user.first_name}: {message.text} | Айди пользователя: {message.from_user.id}"
+                )
+        async for member in app.get_chat_members("-4210829020"):
+            print(member.user.id)
+        
 
 
 asyncio.run(main())
