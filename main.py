5ab3019ec654a2bb1ab0b49da937b21133e93051
RigalHD <profoundromeo@gmail.com>
2024-07-01 14:47:16 +0300

    Refactor(main.py): changed printning of information (bot creates a new text file)

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ api_hash = getenv("api_hash")
 
 async def main():
     async with Client("my_account", api_id, api_hash) as app:
-        async for message in app.get_chat_history("-4210829020"):
-            print(
-                f"{message.from_user.first_name}: {message.text} | Айди пользователя: {message.from_user.id}"
-                )
-        async for member in app.get_chat_members("-4210829020"):
-            print(member.user.id)
-        
-
+        with open("test_.txt", "w", encoding="utf-8") as file:
+            file.write("История сообщений чата: \n")
+            async for message in app.get_chat_history("-4210829020"):
+                file.write(f"{message.from_user.first_name}: {message.text} | Айди пользователя: {message.from_user.id}\n")
+            file.write("Участники чата: \n")
+            async for member in app.get_chat_members("-4210829020"):
+                file.write(f"{member.user.first_name}: | Айди пользователя: {member.user.id}\n")
 
 asyncio.run(main())
